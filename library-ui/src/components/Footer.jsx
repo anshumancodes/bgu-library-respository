494b707da011a90a1aab5acc265371268b23f7f7
anshumancodes <anshumanprof01@gmail.com>
2025-10-17 16:30:57 +0530

    updatr footer

--- library-ui/src/components/Footer.jsx
+++ library-ui/src/components/Footer.jsx
@@ -12,16 +12,13 @@ export default function Footer() {
           </div>
           <div className="footer-section">
             <h3>Support</h3>
-            <div className="footer-links">
-              <a href="#">User Guide</a>
-              <a href="#">FAQ</a>
-            </div>
+           
           </div>
         </div>
         <div className="footer-bottom">
           <span>© 2025 Birla Global University. Powered by DSpace API </span>
-          <span>Developed and Maintained By : <a href="">ape labs</a></span>
-          <div className="coar-badge">🏛️ COAR Notify</div>
+          <span>Developed and Maintained By : <a href="https://www.linkedin.com/in/anshumancdx/">anshumancdx</a></span>
+        
         </div>
       </div>
     </footer>
