8a2ebee86e689840bcef70ca99080bcf247cb701
anshumancodes <anshumanprof01@gmail.com>
2025-10-30 19:38:55 +0530

    feat(utils): add DSpace API helper functions for repository stats

---
+++ library-ui/src/utils/stats.js
@@ -0,0 +1,33 @@
+// src/utils/dspace.js
+const BASE_URL = "http://10.120.4.59:8080/server/api";
+
+async function fetchJSON(url) {
+  const res = await fetch(url, { headers: { Accept: "application/json" } });
+  if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+  return res.json();
+}
+
+
+export async function getTotalItems() {
+  const data = await fetchJSON(`${BASE_URL}/core/communities?page=0&size=1`);
+  return data.page?.totalElements ?? 0;
+}
+
+
+export async function getTotalAuthors() {
+  const data = await fetchJSON(`${BASE_URL}/discover/facets/author`);
+  
+  return data?.values?.length || 0;
+}
+
+
+export async function getTotalCollections() {
+  const data = await fetchJSON(`${BASE_URL}/core/collections?page=0&size=1`);
+  return data.page?.totalElements ?? 0;
+}
+
+
+export async function getTotalDownloads() {
+  const data = await fetchJSON(`${BASE_URL}/discover/facets/subject`);
+  return data?.values?.length || 0;
+}
